f62f6a30ddcebf101862d9e5c7e19334497b76e2
Johann <johanncf@metalmat.ufrj.br>
2021-12-24 16:12:14 -0300

    Correção de texto na descrição do contato

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Contato from './components/Contato';
 function App() {
   return (
     <div className='container'>
-      <Contato foto="https://citacoes.in/media/authors/mark-zuckerberg.jpg" nome="Zuckerberg" descricao="Motherfucker bilionaire." />
-      <Contato foto="https://imagens.brasil.elpais.com/resizer/5DO82rz-Fg0gx_XUcPN4tiQ4D5c=/980x735/cloudfront-eu-central-1.images.arcpublishing.com/prisa/F34R4EEJ2RG3DKUGSVKBLC2GXA.jpeg" nome="Bill Gates" descricao="Another motherfucker bilionaire." />
-      <Contato foto="https://veja.abril.com.br/wp-content/uploads/2016/05/marissa-mayer-google-original6.jpeg" nome="Melissa Mayer" descricao="A female motherfucker bilionaire." />
+      <Contato foto="https://citacoes.in/media/authors/mark-zuckerberg.jpg" nome="Zuckerberg" descricao="Motherfucker billionaire." />
+      <Contato foto="https://imagens.brasil.elpais.com/resizer/5DO82rz-Fg0gx_XUcPN4tiQ4D5c=/980x735/cloudfront-eu-central-1.images.arcpublishing.com/prisa/F34R4EEJ2RG3DKUGSVKBLC2GXA.jpeg" nome="Bill Gates" descricao="Another motherfucker billionaire." />
+      <Contato foto="https://veja.abril.com.br/wp-content/uploads/2016/05/marissa-mayer-google-original6.jpeg" nome="Melissa Mayer" descricao="A female motherfucker billionaire." />
     </div>
   );
 }
